2172988b69fadfbf1c462610de5b442f2baf3e63
Stephen Lauck <stephen.lauck@gmail.com>
2014-05-22 15:08:03 -0700

    initial commit of pipeline cookbook

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'pipeline'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures pipeline'
+long_description 'Installs/Configures pipeline'
+version          '0.1.0'
+
+depends 'jenkins'
---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: pipeline
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'jenkins::java'
+include_recipe 'jenkins::master'
